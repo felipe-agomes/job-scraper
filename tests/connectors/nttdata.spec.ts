1d2a1558a43edd9cf7d545c4e55e96e8525d4db5
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-03-03 18:40:49 -0300

    fix: fix `nttdata.ts` test

---
+++ tests/connectors/nttdata.spec.ts
@@ -0,0 +1,10 @@
+import test from "playwright/test";
+import { loadSingleConnectorConfig } from "../../src/connectors/configLoader";
+import { runJobScraper } from "../../src/scraper/jobScraper";
+
+test("Deve rodar o fluxo completo de raspagem e paginação", async () => {
+  const connectors = loadSingleConnectorConfig("src/connectors/nttdata.yml");
+  const nextiConnector = connectors;
+
+  await runJobScraper(nextiConnector);
+});
