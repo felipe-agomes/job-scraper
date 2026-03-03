0a501f7ee9f5782411473338a4cdff4b1d584019
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-03-03 20:09:16 -0300

    tests: remove `test.only`

--- tests/connectors/nttdata.spec.ts
+++ tests/connectors/nttdata.spec.ts
@@ -2,7 +2,7 @@ import test from "playwright/test";
 import { loadSingleConnectorConfig } from "../../src/connectors/configLoader";
 import { runJobScraper } from "../../src/scraper/jobScraper";
 
-test.only("Deve rodar o fluxo completo de raspagem e paginação", async () => {
+test("Deve rodar o fluxo completo de raspagem e paginação", async () => {
   const connectors = loadSingleConnectorConfig("src/connectors/nttdata.yml");
   const nextiConnector = connectors;
 
