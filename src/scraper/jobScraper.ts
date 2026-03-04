a25aa64459c79dcc8007cdfb04f575d7327ef835
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-03-03 21:59:05 -0300

    feat: add `runJobInfo` function

--- src/scraper/jobScraper.ts
+++ src/scraper/jobScraper.ts
@@ -1,8 +1,8 @@
-import { writeFileSync } from "fs";
 import { chromium } from "playwright";
 import type { ConnectorJobInfo, ConnectorJobList } from "../connectors/types";
-import { executeSteps } from "./engine";
+import { executeStep, executeSteps } from "./engine";
 import { extractAllPages } from "./pagination";
+import { readFileSync, writeFileSync } from "node:fs";
 
 async function runJobList(jobList: ConnectorJobList) {
   const browser = await chromium.launch();
@@ -20,4 +20,26 @@ async function runJobList(jobList: ConnectorJobList) {
   }
 }
 
-export { runJobScraper };
+async function runJobInfo(jobInfo: ConnectorJobInfo) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  const links = JSON.parse(readFileSync(jobInfo.indir, { encoding: "utf8" }));
+  const result: Record<string, string>[] = [];
+  for (const link of links) {
+    await page.goto(link);
+
+    await executeSteps(page, jobInfo.steps);
+
+    for (const info of jobInfo.infos) {
+      const stepResult = await executeStep(page, info);
+
+      if (stepResult)
+        result.push({ [info.action?.value ?? ""]: stepResult.join() });
+    }
+  }
+
+  writeFileSync(jobInfo.outdir, JSON.stringify(result, null, 2));
+}
+
+export { runJobList, runJobInfo };
