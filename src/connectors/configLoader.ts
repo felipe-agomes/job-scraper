ac0ee8f0bf5f61412379be2d91d45ad0ddad8142
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-03-04 12:51:42 -0300

    core: change configloader to compare with jobList

--- src/connectors/configLoader.ts
+++ src/connectors/configLoader.ts
@@ -1,18 +1,34 @@
 import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
 import { parse } from "yaml";
-import type { Connector } from "./types";
+import type { Connector, FindJob } from "./types";
 
-function loadConnectorConfig(basePath: string): Connector[] {
+function loadConnectorConfig(
+  basePath: string,
+  findJobs: FindJob[],
+): Connector[] {
+  const findJobsId = findJobs.map((job) => job.id);
+
   const configs = readdirSync(basePath).filter((conf) => conf.endsWith(".yml"));
 
-  return configs.map((conf) =>
-    parse(readFileSync(join(basePath, conf), { encoding: "utf8" })),
-  );
+  const connectors = configs
+    .map(
+      (conf) =>
+        parse(
+          readFileSync(join(basePath, conf), { encoding: "utf8" }),
+        ) as Connector,
+    )
+    .filter((conn) => findJobsId.includes(conn.id));
+
+  return connectors;
 }
 
 function loadSingleConnectorConfig(path: string): Connector {
   return parse(readFileSync(path, { encoding: "utf8" }));
 }
 
-export { loadConnectorConfig, loadSingleConnectorConfig };
+function loadFindJobConfig(path: string = "find_job.json"): FindJob[] {
+  return JSON.parse(readFileSync(path, { encoding: "utf8" }));
+}
+
+export { loadConnectorConfig, loadSingleConnectorConfig, loadFindJobConfig };
--- tests/connectors/configLoader.spec.ts
+++ tests/connectors/configLoader.spec.ts
@@ -1,8 +1,25 @@
 import test, { expect } from "playwright/test";
-import { loadConnectorConfig } from "../../src/connectors/configLoader";
+import {
+  loadConnectorConfig,
+  loadFindJobConfig,
+  loadSingleConnectorConfig,
+} from "../../src/connectors/configLoader";
 
 test("should bring all connectors config", () => {
-  const configs = loadConnectorConfig("./tests/connectors");
+  const findJob = loadFindJobConfig("tests/find_job.json");
+  const configs = loadConnectorConfig("./tests/connectors", findJob);
 
   expect(configs.length).toBe(1);
+});
+
+test("should bring one connector config", () => {
+  const config = loadSingleConnectorConfig("./tests/connectors/example.yml");
+
+  expect(config.id).toBe("example");
+});
+
+test("should bring one jobConfig", () => {
+  const config = loadFindJobConfig("tests/find_job.json");
+
+  expect(config.length).toBe(1);
 });
